3f31be086594590e852287d2843890ba4faff1d9
Thevindu Hasal Dasanayaka <tdasanay@uwo.ca>
2024-11-16 19:50:39 +0000

    Upload New File

---
+++ Launcher.java
@@ -0,0 +1,17 @@
+package petopia_2212;
+
+import javax.swing.SwingUtilities;
+
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){ // runs everything on the same thread
+		@Override
+			public void run() {
+				MainMenu main = new MainMenu();
+				
+			}
+		});
+	}
+}

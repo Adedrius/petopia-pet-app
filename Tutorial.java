cc4bed920f0302b2357f3ecfcac8ddb9ec8beb72
Ade Anthony-Egorp <aanthon8@uwo.ca>
2024-11-20 18:52:52 -0500

    Re-wrote the code for the Tutorial page.

--- Tutorial.java
+++ Tutorial.java
@@ -1,35 +1,32 @@
-import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.*;
 
 public class Tutorial extends JFrame {
-    private JTextArea Title;
-    private JButton mainMenu;
-    
     public Tutorial() {
+        
         setTitle("Instructions Menu");
-        setSize(500,400);
+        setSize(500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setLayout(null); 
 
-        String tutorialTitle = "Game Instructions Video";
-
-        Title = new JTextArea(tutorialTitle);
-        Title.setLineWrap(true);
-        Title.setWrapStyleWord(true);
-        Title.setEditable(false);
-
-        JScrollPane scrollPane = new JScrollPane(Title);
-
-        mainMenu = new JButton("Back to Main Menu");
         
-        setLayout(new BorderLayout());
-        add(scrollPane, BorderLayout.CENTER);
-        add(mainMenu, BorderLayout.SOUTH);
+        JTextArea Title = new JTextArea("Game Instructions Video");
+        Title.setBounds(130, 25, 225, 30); // x, y, width, height
+        Title.setFont(new Font("Arial", Font.PLAIN, 20));
+        Title.setOpaque(false);
+        Title.setBorder(null);
+        Title.setEditable(false); // Make the text area non-editable
+        add(Title); 
+
+        
+        JButton mainMenu = new JButton("Back to Main Menu");
+        mainMenu.setBounds(165, 290, 150, 30); // x, y, width, height
+        add(mainMenu); 
 
         setVisible(true);
-
-    }   
+    }
 
     public static void main(String[] args) {
         new Tutorial();
